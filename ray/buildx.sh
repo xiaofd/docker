a151d7008a1b39bc4fba969de8a3abbab736843a
xiaofd <jun@jun.ac.cn>
2022-05-02 16:19:03 +0000

    ray

--- ray/buildx.sh
+++ ray/buildx.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# docker run -it --rm --privileged tonistiigi/binfmt --install all #  to install qemu emulators
 # docker buildx create --use --platform linux/amd64,linux/arm64,linux/arm/v6,linux/arm/v7,linux/386 --buildkitd-flags '--allow-insecure-entitlement network.host'
 docker buildx build -t xiaofd/ray -f Dockerfile --platform linux/amd64,linux/arm64,linux/arm/v6,linux/arm/v7,linux/386 -o type=registry .
 # docker buildx build -t xiaofd/ray -f Dockerfile --platform linux/amd64,linux/arm64,linux/386 -o type=registry .
