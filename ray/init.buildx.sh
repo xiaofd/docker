5e5ac6d4b5e4e40c456edcc48d4b93d171e3cd1c
xiaofd <jun@jun.ac.cn>
2023-11-25 08:36:16 +0800

    .

--- ray/init.buildx.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker run -it --rm --privileged tonistiigi/binfmt --install all #  to install qemu emulators
-docker buildx create --use --platform linux/amd64,linux/arm64,linux/arm/v6,linux/arm/v7,linux/386 --buildkitd-flags '--allow-insecure-entitlement network.host'
-
